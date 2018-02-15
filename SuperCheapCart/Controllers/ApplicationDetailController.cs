2c52963d4bf3495921925f8bd227089c34715470
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2018-02-15 12:24:48 +0800

    Update ApplicationDetailController.cs

--- SuperCheapCart/Controllers/ApplicationDetailController.cs
+++ SuperCheapCart/Controllers/ApplicationDetailController.cs
@@ -46,5 +46,75 @@ namespace SuperCheapCart.Controllers
             }
             return applicationDetailList;
         }
+                [Route("api/getApplicationList")]
+        [HttpGet]
+        public IList<ApplicationDetail> getApplication([FromUri] ApplicationDetail searchFilter)
+        {
+            string conditons = "";
+            if (searchFilter != null)
+            {
+                conditons = makeQuery(searchFilter);
+            }
+
+            List<ApplicationDetail> applicationDetailList = new List<ApplicationDetail>();
+            using (MySqlConnection conn = new MySqlConnection(connection.MySQLConnectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand((@"SELECT apd.applicationId, apd.applicationStatusId,
+                                                              apd.applicationTypeId, apd.customerId  FROM myloanworld.applicationDetail as apd " + conditons), conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            applicationDetailList.Add(new ApplicationDetail()
+                            {
+                                ApplicationId = Convert.ToInt16(reader["applicationId"]),
+                                ApplicationStatusId = Convert.ToInt16(reader["applicationStatusId"]),
+                                //ApplicationStatus = reader["Application Status"].ToString(),
+                                ApplicationTypeId = Convert.ToInt16(reader["applicationTypeId"]),
+                                //ApplicationType = reader["Application Type"].ToString(),
+                                //CustomerName = reader["Customer Name"].ToString()
+                            });
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return applicationDetailList;
+        }
+
+        private string makeQuery(ApplicationDetail searchFilter)
+        {
+            IList<string> conditionList = new List<string>();
+            string query = "where ";
+            foreach (var prop in searchFilter.GetType().GetProperties())
+            {
+                if (prop.GetValue(searchFilter, null) != null)
+                {
+                    if ((prop.PropertyType.FullName == "System.Int32") && ((int)prop.GetValue(searchFilter, null) != 0))
+                    {
+                        conditionList.Add(prop.Name + "=" + prop.GetValue(searchFilter, null));
+                    }
+                    else if ((prop.PropertyType.FullName == "System.String") && (prop.GetValue(searchFilter, null).ToString() != ""))
+                    {
+                        conditionList.Add(prop.Name + "='" + prop.GetValue(searchFilter, null) + "'");
+                    }
+                }
+            }
+            if (conditionList.Count > 1)
+            {
+                foreach (string condition in conditionList)
+                {
+                    query += condition + " or ";
+                }
+            }
+            else if (conditionList.Count == 1)
+            {
+                query += conditionList[0];
+            }
+            string queryWithoutEnd = query.Substring(0, query.LastIndexOf(" or "));
+            return queryWithoutEnd;
+        }
     }
 }
